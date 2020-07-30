b62b5d41b9a42cac4d9d19fc4ae5176482482918
lorix <foxred324@gmail.com>
2020-07-30 16:30:40 +0300

    proxy a tcp client

---
+++ proxy_a_tcp_client/main.go
@@ -0,0 +1,37 @@
+package main
+
+func handle() {
+
+	dst, err := net.Dial("tcp", "joescatcam.website:80")
+
+	if err != nil {
+		log.Fatalln("Unable to connect to our unreadeble host")
+	}
+
+	defer dst.Close()
+
+	go func() {
+		if _, err := io.Copy(dst, src); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	if _, err := io.Copy(src, dst); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":80")
+	if err != nil {
+		log.Fatalln("Unable to bind to port")
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatalln("Unable to accept connection")
+		}
+		go handle(conn)
+	}
+}
