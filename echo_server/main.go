3cfbac26d176f837234d5b4ec0e7cc9db6f0f568
lorix <foxred324@gmail.com>
2020-07-30 15:04:20 +0300

    improving the code by creating a buffered Listener v.1

--- echo_server/main.go
+++ echo_server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"io"
 	"log"
 	"net"
@@ -9,36 +10,35 @@ import (
 func echo(conn net.Conn) {
 	defer conn.Close()
 
-	b := make([]byte, 512)
-	for {
-		size, err := conn.Read(b[0:])
-		if err == io.EOF {
-			log.Println("Client disconnected")
-			break
-		}
-		if err != nil {
-			log.Println("Unexpected error")
-			break
-		}
-		log.Printf("Recived %d bytes: %s\n", size, string(b))
-
-		log.Println("Write data")
-		if _, err := conn.Write(b[0:size]); err != nil {
-			log.Fatalln("Unable to write data")
-		}
+	reader := bufio.NewReader(conn)
+	s, err := reader.ReadString('\n')
+	if err == io.EOF {
+		log.Println("Client disconnected")
 	}
+	if err != nil {
+		log.Println("Unexpected error")
+	}
+	log.Printf("Recived %d bytes: %s\n", len(s), s)
+
+	log.Println("Write data")
+	writer := bufio.NewWriter(conn)
+	if _, err := writer.WriteString(s); err != nil {
+		log.Fatalln("Unable to write data")
+	}
+	writer.Flush()
+
 }
 
 func main() {
-	
+
 	listener, err := net.Listen("tcp", ":20080")
-	
+
 	if err != nil {
 		log.Fatalln("Unabel to bind to port")
 	}
-	
+
 	log.Println("Listening on 0.0.0.0:20080")
-	
+
 	for {
 		conn, err := listener.Accept()
 		log.Println("Recived connection")
