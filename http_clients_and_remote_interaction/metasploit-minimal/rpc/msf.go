6ad839a30ad09891532002166c874dd8e1d291ce
lorix <foxred324@gmail.com>
2020-08-31 15:59:37 +0300

    describe login-logout structure

--- http_clients_and_remote_interaction/metasploit-minimal/rpc/msf.go
+++ http_clients_and_remote_interaction/metasploit-minimal/rpc/msf.go
@@ -20,3 +20,30 @@ type sessionListRes struct {
 	UUID        string `msgpack:"uuid"`
 	ExploitUUID string `msgpack:"exploit_uuid"`
 }
+
+type loginReq struct {
+	_msgpack struct{} `msgpack:",asArray"`
+	Method   string
+	Username string
+	Password string
+}
+
+type loginRes struct {
+	Result     string `msgpack:"result"`
+	Token      string `msgpack:"token"`
+	Error      bool   `msgpack:"error"`
+	ErrorClass string `msgpack:"error_class"`
+
+	ErrorMessage string `msgpack:"error_message"`
+}
+
+type logoutReq struct {
+	_msgpack    struct{} `msgpack:",asArray"`
+	Method      string
+	Token       string
+	LogoutToken string
+}
+
+type logoutRes struct {
+	Result string `msgpack:"result"`
+}
