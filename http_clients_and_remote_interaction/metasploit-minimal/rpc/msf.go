37f1d46b22d23a2c826d5a58e6e1802115ba55dd
lorix <foxred324@gmail.com>
2020-09-02 12:24:26 +0300

    login-logout flow

--- http_clients_and_remote_interaction/metasploit-minimal/rpc/msf.go
+++ http_clients_and_remote_interaction/metasploit-minimal/rpc/msf.go
@@ -14,7 +14,7 @@ type sessionListReq struct {
 	Token    string
 }
 
-type sessionListRes struct {
+type SessionListRes struct {
 	ID          uint32 `msgpack:",omitepty"`
 	Type        string `msgpack:"type"`
 	TunnelLocal string `msgpack:"tunnel_local"`
@@ -65,17 +65,22 @@ type Metasploit struct {
 	token string
 }
 
-func New(host, user, pass string) *Metasploit {
+func New(host, user, pass string) (*Metasploit, error) {
 	msf := &Metasploit{
 		host: host,
 		user: user,
 		pass: pass,
 	}
-	return msf
+
+	if err := msf.Login(); err != nil {
+		return nil, err
+	}
+
+	return msf, nil
 }
 
 func (msf *Metasploit) send(req interface{}, res interface{}) error {
-	
+
 	buf := new(bytes.Buffer)
 	msgpack.NewEncoder(buf).Encode(req)
 	dest := fmt.Sprintf("http://%s/api", msf.host)
@@ -84,11 +89,61 @@ func (msf *Metasploit) send(req interface{}, res interface{}) error {
 	if err != nil {
 		return err
 	}
-	defer r. body.Close()
+	defer r.body.Close()
 
 	if err := msgpack.NewDecoder(r.Body).Decode(&res); err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func (msf *Metasploit) Login() error {
+	ctx := &loginReq{
+		Method:   "auth.login",
+		Username: msf.user,
+		Password: msf.pass,
+	}
+
+	var res loginRes
+	if err := msf.send(ctx, &res); err != nil {
+		return err
+	}
+
+	msf.token = res.Token
+	return nil
+}
+
+func (msf *Metasploit) Logout() error {
+	ctx := &logoutReq{
+		Method:      "auth.logout",
+		Token:       msf.token,
+		LogoutToken: msf.token,
+	}
+
+	var res logoutRes
+	if err := msf.send(ctx, &res); err != nil {
+		return err
+	}
+	msf.token = ""
+	return nil
+}
+
+func (msf *Metasploit) SessionList() (map[uint32]SessionListRes, error) {
+	req := &sessionListReq{
+		Method: "session.list",
+		Token:  msf.token,
+	}
+
+	res := make(map[uint32]SessionListRes)
+	if err := msf.send(req, &res); err != nil {
+		return nil, err
+	}
+
+	for id, session := range res {
+		session.ID = id
+		res[id] = session
+	}
+
+	return res, nil
 }
